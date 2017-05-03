b4452a35ab1734bc3f4e0d1547e836569df16d8e
Jonathon Lin <jonathonlin93@yahoo.com>
2017-05-03 16:05:11 -0700

    styesheet test

---
+++ styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  word: {
+    fontSize: 100,
+    color: 'red',
+  }
+});
+
+export default styles;
