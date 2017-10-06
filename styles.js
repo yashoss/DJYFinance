3e13d1b3152f216648ac37a04328643239318546
Jonathon Lin <jonathonlin93@yahoo.com>
2017-10-06 14:55:59 -0700

    finish convert currency

--- styles.js
+++ styles.js
@@ -1,34 +1,36 @@
 import { StyleSheet } from 'react-native';
 
+const WIDTH = 200;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-    landingContainer: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#EDEDED',
-    },
-      header: {
-        fontSize: 40,
-        color: 'black',
-      },
-        picker: {
-          width: 200,
-          height: 80,
-          backgroundColor: '#FFF',
-          marginBottom: 10
-        },
-        textInput: {
-          alignSelf: 'center',
-          height: 30,
-          width: 200,
-          textAlign: 'center',
-          backgroundColor: '#FFF',
-          borderWidth: 0.5,
-          borderColor: '#C0C0C0',
-        },
+  landingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#EDEDED',
+  },
+  header: {
+    fontSize: 40,
+    color: 'black',
+  },
+  picker: {
+    width: WIDTH,
+    height: 80,
+    backgroundColor: '#FFF',
+    marginBottom: 10
+  },
+  textInput: {
+    alignSelf: 'center',
+    height: 30,
+    width: WIDTH,
+    textAlign: 'center',
+    backgroundColor: '#FFF',
+    borderWidth: 0.5,
+    borderColor: '#C0C0C0',
+  },
 });
 
 export default styles;
