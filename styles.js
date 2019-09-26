6b2738e1b639c0de3013077410c5ab9714a22d76
Jonathon Lin <jonathonlin93@yahoo.com>
2019-09-25 22:56:05 -0500

    undo color change

--- styles.js
+++ styles.js
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
     height: 30,
     width: WIDTH,
     textAlign: 'center',
-    backgroundColor: '#FFE',
+    backgroundColor: '#FFF',
     borderWidth: 0.5,
     borderColor: '#C0C0C0',
   },
